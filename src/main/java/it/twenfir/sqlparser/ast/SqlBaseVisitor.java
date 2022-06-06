e2bc828251eaa74b5d807657282667e081d5d019
Nicola Musatti <nicola.musatti@gmail.com>
2022-06-06 18:43:07 +0200

    Visit newly added classes

--- src/main/java/it/twenfir/sqlparser/ast/SqlBaseVisitor.java
+++ src/main/java/it/twenfir/sqlparser/ast/SqlBaseVisitor.java
@@ -5,12 +5,77 @@ import it.twenfir.antlr.ast.BaseAstVisitor;
 public abstract class SqlBaseVisitor<ValueT> extends BaseAstVisitor<ValueT> implements SqlVisitor<ValueT> {
 
     @Override
-    public ValueT visit(Statement node) {
+    public ValueT visit(CombinedInputParameter node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(CombinedOutputParameter node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(Expression node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(Factor node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(Indicator node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(InputParameter node) {
         return visitChildren(node);
     }
 
     @Override
     public ValueT visit(IntoClause node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(OutputParameter node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(Parameter node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(SelectExpression node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(SelectStatement node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(SimpleSelect node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(Statement node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(Term node) {
+        return visitChildren(node);
+    }
+
+    @Override
+    public ValueT visit(WhereClause node) {
         return visitChildren(node);
     }
     
--- src/main/java/it/twenfir/sqlparser/ast/SqlVisitor.java
+++ src/main/java/it/twenfir/sqlparser/ast/SqlVisitor.java
@@ -3,6 +3,19 @@ package it.twenfir.sqlparser.ast;
 import it.twenfir.antlr.ast.AstVisitor;
 
 public interface SqlVisitor<ValueT> extends AstVisitor<ValueT> {
-    ValueT visit(Statement node);
+    ValueT visit(CombinedInputParameter node);
+    ValueT visit(CombinedOutputParameter node);
+    ValueT visit(Expression node);
+    ValueT visit(Factor node);
+    ValueT visit(Indicator node);
+    ValueT visit(InputParameter node);
     ValueT visit(IntoClause node);
+    ValueT visit(OutputParameter node);
+    ValueT visit(Parameter node);
+    ValueT visit(SelectExpression node);
+    ValueT visit(SelectStatement node);
+    ValueT visit(SimpleSelect node);
+    ValueT visit(Statement node);
+    ValueT visit(Term node);
+    ValueT visit(WhereClause node);
 }
